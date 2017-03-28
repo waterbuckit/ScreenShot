3c5497bfe46b7ef91777ada76fd29723ee152444
aroyal1 <aroyal1@A1-15.SHS.Local>
2017-03-28 17:14:12 +0100

    Added documentation

--- src/screenshotcheck/ScreenShotPolygon.java
+++ src/screenshotcheck/ScreenShotPolygon.java
@@ -25,7 +25,9 @@ public class ScreenShotPolygon {
 
     private final JWindow screen;
     private final PanelDrag drawOn;
-
+    /**
+     * Constructor for main object, sets up GUI elements.
+     */
     public ScreenShotPolygon() {
         screen = new JWindow();
         drawOn = new PanelDrag();
@@ -37,49 +39,76 @@ public class ScreenShotPolygon {
         screen.setBackground(new Color(0, 0, 0, 50));
         drawOn.setOpaque(false);
     }
-
+    
+    /**
+     * Main method for initialising the screenShot class.
+     * @param args 
+     */
     public static void main(String[] args) {
         ScreenShotPolygon screenShotter = new ScreenShotPolygon();
         screenShotter.screen.setVisible(true);
     }
-
+    
+    /**
+     * Class for manipulating the line object
+     */
     private static class MyLine {
-
         Point previousClicked;
         Point currentMousePos;
 
+        /**
+         * creates a new MyLine, takes only clicked point as the current mouse position
+         * at the start will be the point clicked.
+         * @param clickedPoint 
+         */
         public MyLine(Point clickedPoint) {
             this.previousClicked = clickedPoint;
             this.currentMousePos = clickedPoint;
         }
-
+        /**
+         * sets the current mouse position
+         * @param currentMousePos 
+         */
         private void setCurrentMousePos(Point currentMousePos) {
             this.currentMousePos = currentMousePos;
         }
+        /**
+         * sets the previous clicked point
+         * @param previousClicked 
+         */
         private void setPreviousClicked(Point previousClicked){
             this.previousClicked = previousClicked;
         }
+        
+        /**
+         * Draw method for drawing the current instance of the MyLine
+         * @param g2d 
+         */
         private void draw(Graphics2D g2d) {
             g2d.drawLine(previousClicked.x, previousClicked.y,
                     currentMousePos.x, currentMousePos.y);
         }
-
     }
 
     /**
-     * @param args the command line arguments
+     * Custom JPanel for drawing on.
      */
     class PanelDrag extends JPanel {
 
         private Shape s;
         private MyLine currentLine;
-
+        /**
+         * constructor sets up the listeners.
+         */
         public PanelDrag() {
             MouseControl madapt = new MouseControl();
             addMouseMotionListener(madapt);
             addMouseListener(madapt);
         }
-
+        /**
+         * Allows to paint custom objects to screen
+         * @param g 
+         */
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
@@ -93,9 +122,15 @@ public class ScreenShotPolygon {
                 s.draw(g2d);
             }
         }
-
+        /**
+         * custom MouseAdapter for handling mouse events
+         */
         private class MouseControl extends MouseAdapter {
-
+            /**
+             * Takes a mouse event and uses that to decide whether to create a new
+             * shape or simply add a new point to the shape
+             * @param me 
+             */
             @Override
             public void mousePressed(MouseEvent me) {
                 if (SwingUtilities.isLeftMouseButton(me)) {
@@ -111,7 +146,10 @@ public class ScreenShotPolygon {
                     System.exit(0);
                 }
             }
-
+            /**
+             * Updates the current position of the mouse on the MyLine object
+             * @param me 
+             */
             @Override
             public void mouseMoved(MouseEvent me) {
                 if (currentLine != null) {
@@ -135,26 +173,41 @@ public class ScreenShotPolygon {
         }
     }
 
-    // takes a clicke
+    /**
+     * shape class for drawing all the points of the polygon.
+     */
     class Shape {
 
         Point clickedPoint;
         ArrayList<Point> pointsClicked;
-
+        /**
+         * Constructor makes a new list of points that have already been clicked.
+         */
         public Shape() {
             pointsClicked = new ArrayList<>();
         }
 
-        // adds a new point 
+        /**
+         * adds new points to the to-be polygon.
+         * @param addedPoint 
+         */
         private void addPoint(Point addedPoint) {
             pointsClicked.add(addedPoint);
         }
-
+        /**
+         * returns the previous point.
+         * @return 
+         */
         private Point getPreviousPoint() {
             return pointsClicked.get(pointsClicked.size() - 1);
         }
 
-        //draw lines to all the points
+        /**
+         * Draws all the previous lines by iterating over the list, drawing in 
+         * form of (current line in list.x, current line in list.y, previous line in list.x, 
+         * previous line in list.y
+         * @param g2d 
+         */
         void draw(Graphics2D g2d) {
             for (int i = 0; i < pointsClicked.size() - 1; i++) {
                 g2d.drawLine(pointsClicked.get(i).x, pointsClicked.get(i).y,
